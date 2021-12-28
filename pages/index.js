9c1cfb27a055c91653b14af835a6a17cd6079cc9
abhijeet <abhijeet.dbritto@yapsody.com>
2021-12-28 11:41:10 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Abhijeet" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Something is cooking ...
         </p>
       </main>
 
