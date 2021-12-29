4a6281538e740071793b94772ee03727ae8adcc3
abhijeet <abhijeet.dbritto@yapsody.com>
2021-12-29 11:44:25 +0530

    gif added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+
+
 export default function Home() {
   return (
     <div className="container">
@@ -9,14 +11,14 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
         <Header title="Abhijeet" />
         <p className="description">
           Something is cooking ...
         </p>
+        <img src="/Running deer.gif"/>
       </main>
-
+      
       <Footer />
     </div>
   )
